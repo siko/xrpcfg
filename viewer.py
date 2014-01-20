4245b79f67506fe272cd93f5996dc0c6c55a6543
Siko Chen <sikocb@gmail.com>
2014-01-20 16:01:26 +0800

    init project of get today xrp earned

---
+++ viewer.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# install the requiremnts,modfiy the wcg account id of the wcgid below.
+# pip install lxml
+# pip install requests
+# pip install beautifulsoup4
+
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+def getpoint(url):
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+
+    rows = soup.find('table').findAll('tr')[1:]
+    for row in rows:
+        columns = [data.text for data in row.findAll('td')[0:-1]]
+        todays = datetime.datetime.now().strftime('%b %d, %Y')
+        if columns[0] == todays:
+            print columns[0]+'\t\t'+columns[1]+'\t\t'+columns[2]
+
+cfgurl = 'https://www.computingforgood.org/stats/member/'
+wcgid = [866331,866332,866333,866334]
+
+for w in wcgid:
+    getpoint(cfgurl+str(w))
+
+
